861a11ea84a9eb13643406b9ee835cf0b9398e54
TylerTheLoser <43252312+TylerTheLoser@users.noreply.github.com>
2019-11-06 17:30:07 -0500

    Add files via upload
    added source code.

---
+++ source/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace MTG_App
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Item //this class will deserialize the json key that the WebClient retrieves.
+        {
+            //these are the only three keys for the authentication
+            public string token { get; set; } //get the value of the token key
+            public string message { get; set; } //get the value of the message key
+            public string status { get; set; } //get the value of the status key
+        }
+
+        //we use an auto implementation property to get the values of the email and password outside this method
+        //this will GET the yourEmail/yourPass string that is SET elsewhere (see below)
+        string yourEmail { get; set; }
+        string yourPass { get; set; }
+        public string authToken { get; set; }
+
+        //the authentication method
+        public void start_get()
+        {
+            //since i know this throws an error if the email/pass combo doesn't work, i nested it in a try/catch
+            //basically, a try will TRY the code you nest in it, and if it CATCHES the error specified below, it won't crash the program and throw the code you put in place IF the error happens!
+            try
+            {
+                //this downloads the info from the given URL
+                //it's concatenated with the yourEmail and yourPass values that are GET and SET elsewhere!
+                string json = new WebClient().DownloadString("https://www.echomtg.com/api/user/auth/email=" + yourEmail + "&type=curl&password=" + yourPass);
+                
+                //this deserializes the JSON keys that are specified in the Item class for the URL downloaded above
+                Item items = JsonConvert.DeserializeObject<Item>(json);
+
+                //more of a debug thing here...
+                //i wrote this to get a response in the console window to ensure i'm getting the values back
+                Console.WriteLine("Your token is " + items.token.ToString());
+                Console.WriteLine("message: " + items.message.ToString());
+                Console.WriteLine("status: " + items.status.ToString());
+
+                //this initializes a new instance of the MainWindow
+                MainWindow mainwin = new MainWindow();
+
+                mainwin.dumbToken = items.token.ToString(); //gives value to a variable in MainWindow
+
+                //TODO CLOSE THE WINDOW
+                
+                //this opens the MainWindow
+                mainwin.Show();
+                this.Hide(); //shitty lol
+            }
+            //since i know the error that happens when the credentials are wrong, i catch the error before it crashes my program
+            catch (System.NullReferenceException)
+            {
+                //just a dumb way of showing you got an error.
+                MessageBox.Show("Authentication Error. Please try your email/password again...", "Authentication Error");
+            }
+                   
+        }
+        //the shit that happens when you click the button
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            yourEmail = txtEmail.Text; //this is where the yourEmail string is SET!
+            yourPass = txtPassword.Text; //this is where yourPass is SET!
+            if (String.IsNullOrEmpty(txtEmail.Text)) //if the the textbox value is null or empty...
+            {
+                MessageBox.Show("Please enter an email...", "Missing email"); //throw the error
+            } else if (String.IsNullOrEmpty(txtPassword.Text)) { //if the textbox value HERE is null or empty...
+                MessageBox.Show("Please enter a password...", "Missing password"); //throw the error
+            } else //otherwise...
+            {
+                start_get(); //run the authentication method
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://www.echomtg.com/register/");
+            Process.Start(sInfo);
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://www.echomtg.com/user/forgot_password/");
+            Process.Start(sInfo);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ source/MainWindow.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace MTG_App
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string dumbToken; //define the variable
+
+        public void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var searchTerm = txtSearch.Text;
+            var labelMulti = lblMID.Text;
+            var labelEcho = lblEID.Text;
+            var labelName = lblTheName.Text;
+            var labelExp = lblExpansion.Text;
+            var labelSet = lblSC.Text;
+
+            var json = new WebClient().DownloadString("https://www.echomtg.com/api/data/card_reference/auth=" + dumbToken);
+            var myCardRef = CardRef.FromJson(json);
+            Console.WriteLine(myCardRef.Message);
+            Console.WriteLine(myCardRef.Status);
+            Console.WriteLine(myCardRef.Cards.Count);
+            try
+            {
+                foreach (var item in myCardRef.Cards.Keys)
+                {
+                    if (myCardRef.Cards[item].Name.Equals(searchTerm, StringComparison.InvariantCultureIgnoreCase)) //TODO check if null
+                                                                                                                    //TODO iterate through multiple responses. this is important!
+                    {
+                        string imagelink = ("https://assets.echomtg.com/magic/cards/original/" + myCardRef.Cards[item].EchoId.ToString() + ".jpg");
+                        picBox.ImageLocation = imagelink;
+                        picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        lblTheName.Text = myCardRef.Cards[item].Name;
+                        lblMID.Text = myCardRef.Cards[item].MultiverseId.ToString();
+                        lblEID.Text = myCardRef.Cards[item].EchoId.ToString();
+                        lblExpansion.Text = myCardRef.Cards[item].Expansion.ToString();
+                        lblSC.Text = myCardRef.Cards[item].SetCode.ToString();
+                        break;
+                    }
+                }
+            } catch(NullReferenceException)
+            {
+                MessageBox.Show("Card not found, please enter a valid card name.", "Error");
+            }
+
+        }
+
+        private void btnGetInv_Click(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://www.echomtg.com/api/inventory/view/start=0&limit=100&auth=" + dumbToken);
+            var myInventory = MyInventory.FromJson(json);
+            var test = JObject.Parse(json);
+            JArray itemcount = (JArray)test["items"];
+            int length = itemcount.Count;
+            Console.WriteLine(length);
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            int i = 0;
+            while (i < length)
+            {
+                //TODO add to dgv
+                var rowindex = dataGridView1.Rows.Add();
+                dataGridView1.Rows[rowindex].Cells["cardName"].Value = myInventory.Items[i].Name.ToString();
+                dataGridView1.Rows[rowindex].Cells["inventoryID"].Value = myInventory.Items[i].InventoryId.ToString();
+                dataGridView1.Rows[rowindex].Cells["rarity"].Value = myInventory.Items[i].Rarity.ToString();
+                dataGridView1.Rows[rowindex].Cells["condition"].Value = myInventory.Items[i].Condition.ToString();
+                dataGridView1.Rows[rowindex].Cells["set"].Value = myInventory.Items[i].Set.ToString();
+                dataGridView1.Rows[rowindex].Cells["expansion"].Value = myInventory.Items[i].Expansion.ToString();
+                dataGridView1.Rows[rowindex].Cells["colors"].Value = myInventory.Items[i].Colors.ToString();
+                dataGridView1.Rows[rowindex].Cells["types"].Value = myInventory.Items[i].Types.ToString();
+                dataGridView1.Rows[rowindex].Cells["currentprice"].Value = myInventory.Items[i].CurrentPrice.ToString();
+                dataGridView1.Rows[rowindex].Cells["dateacquired"].Value = myInventory.Items[i].DateAcquired.ToString();
+                i++;
+            }
+        }
+
+        private void btnCSV_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV|*.csv";
+            sfd.Title = "Save your CSV file";
+            sfd.FileName = "MTG_Inventory.csv";
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                var sb = new StringBuilder();
+
+                var headers = dataGridView1.Columns.Cast<DataGridViewColumn>();
+                sb.AppendLine(string.Join(",", headers.Select(column => "\"" + column.HeaderText + "\"").ToArray()));
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    var cells = row.Cells.Cast<DataGridViewCell>();
+                    sb.AppendLine(string.Join(",", cells.Select(cell => "\"" + cell.Value + "\"").ToArray()));
+                }
+                System.IO.StreamWriter file = new System.IO.StreamWriter(Path.GetFullPath(sfd.FileName).ToString());
+                file.WriteLine(sb.ToString());
+                file.Close();
+                MessageBox.Show("File saved successfully.", "File Saved");
+            }
+
+        }
+
+        private void btnAddInv_Click(object sender, EventArgs e)
+        {
+            if(lblEID.Text != null)
+            {
+                //TODO: use a better reference, allow the user to change the condition, acquire date, and quantity. etc. this is too simple.
+                var json = new WebClient().DownloadString("https://www.echomtg.com/api/inventory/add/mid=" + lblMID.Text.ToString() + "&auth=" + dumbToken);
+                MessageBox.Show("Successfully added " + lblTheName.Text + " to your inventory!", "Card Added!");
+            } else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+
+}
